572cab45c32541899a8dc042c176574d94263d2f
maxd66 <max.dunn63@gmail.com>
2021-07-14 23:54:42 -0700

    began logic for post request to api/notes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
@@ -11,5 +16,12 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.
 app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/css/styles.css')));
 
 app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/js/index.js')));
+
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
+
+app.post('/api/notes', (req, res) => {
+    const newTask = req.body
+    
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
