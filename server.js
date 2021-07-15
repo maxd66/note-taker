20840e36759da3b25a7d5023b0a3f8a5d8565d8e
maxd66 <max.dunn63@gmail.com>
2021-07-14 23:33:48 -0700

    added server.js and completed get requests for webpages and their js and css

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/css/styles.css')));
+
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/js/index.js')));
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
