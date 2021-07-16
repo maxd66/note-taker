ac6e72be109bad81671d3861bb13284bd805c241
maxd66 <max.dunn63@gmail.com>
2021-07-15 19:58:16 -0700

    added proper server file structure, began to transfer get requests to htmlrequests js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = function (app) {
+    
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+    app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, '../public/assets/css/styles.css')));
+
+    app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, '../public/assets/js/index.js')));
+
+}
--- server.js
+++ server.js
@@ -4,24 +4,19 @@ const path = require('path');
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-
-app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/css/styles.css')));
-
-app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'public/assets/js/index.js')));
+require('./routes/htmlRoutes.js')
+require('./routes/apiRoutes.js')
 
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
 
 app.post('/api/notes', (req, res) => {
     const newTask = req.body
-    
+
 })
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
